eb2c9c3ac05543ce24f18f0cca63b1ce0afd888c
Devan0005 <gamingspider0005@gmail.com>
2025-08-14 20:40:17 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Fill these with your Firebase project settings from the Firebase console
+// https://console.firebase.google.com/ -> Project settings -> Your apps -> Config
+window.FIREBASE_CONFIG = {
+	apiKey: 'AIzaSyBf-MBe0XnoTK7JC-1moOclROwBhuZ2qA',
+	authDomain: 'jarvis-6bb85.firebaseapp.com',
+	projectId: 'jarvis-6bb85',
+	storageBucket: 'jarvis-6bb85.appspot.com',
+	messagingSenderId: '517738776263',
+	appId: '1:517738776263:web:5bc00060b12100637e20e2',
+	measurementId: 'G-VDJZW205ME'
+}; 
+
